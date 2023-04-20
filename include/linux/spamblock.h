26da2f52eb9924846a0966d89710a0cb03e23b42
roynatech2544 <whiteshell2544@naver.com>
2023-04-20 09:41:57 +0000

    ASoC: abox: No-op logspam and PM QOS

---
+++ include/linux/spamblock.h
@@ -0,0 +1,22 @@
+#ifndef SPAMBLOCK_H
+#define SPAMBLOCK_H
+
+#define STUB(type, name) static inline void vararg_discard_##name(type t, ...) {}
+#define CALL_STUB(name) vararg_discard_##name
+
+#include <linux/device.h>
+STUB(struct device *, device)
+#undef dev_info
+#undef dev_dbg
+#define dev_info(dev, ...) CALL_STUB(device)(dev, __VA_ARGS__)
+#define dev_dbg(dev, ...) CALL_STUB(device)(dev, __VA_ARGS__)
+
+#include <linux/printk.h>
+STUB(const char *, format)
+#undef pr_info
+#define pr_info(fmt, ...) CALL_STUB(format)(fmt, __VA_ARGS__)
+
+#include <linux/pm_qos.h>
+#define pm_qos_update_request(target, freq) (void)0
+#define pm_qos_request(qos_class) 0
+#endif

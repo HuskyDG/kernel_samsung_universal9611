20b42e58a71905223d1cc3fc13cf98759539b3a1
roynatech2544 <whiteshell2544@naver.com>
2023-04-21 03:01:07 +0000

    spamblock: stringfy varargs

--- include/linux/spamblock.h
+++ include/linux/spamblock.h
@@ -8,13 +8,13 @@
 STUB(struct device *, device)
 #undef dev_info
 #undef dev_dbg
-#define dev_info(dev, ...) CALL_STUB(device)(dev, __VA_ARGS__)
-#define dev_dbg(dev, ...) CALL_STUB(device)(dev, __VA_ARGS__)
+#define dev_info(dev, ...) CALL_STUB(device)(dev, ##__VA_ARGS__)
+#define dev_dbg(dev, ...) CALL_STUB(device)(dev, ##__VA_ARGS__)
 
 #include <linux/printk.h>
 STUB(const char *, format)
 #undef pr_info
-#define pr_info(fmt, ...) CALL_STUB(format)(fmt, __VA_ARGS__)
+#define pr_info(fmt, ...) CALL_STUB(format)(fmt, ##__VA_ARGS__)
 
 #include <linux/pm_qos.h>
 #define pm_qos_update_request(target, freq) (void)0
